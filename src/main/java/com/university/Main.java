21d26a552edbe99257acfc4359f1054e5c8f57a3
shikigami <mazenkassem101@gmail.com>
2026-06-30 15:20:11 +0300

    project structure

---
+++ src/main/java/com/university/Main.java
@@ -0,0 +1,8 @@
+package main.java.com.university;
+
+class Main {
+    public static void main(String[] args){
+        System.out.println("Initializing University Management System Engine...");
+
+    }
+}
