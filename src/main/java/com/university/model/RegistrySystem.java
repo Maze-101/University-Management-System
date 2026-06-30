1b0c20e103ab0f1cf7640dafa5c90409a4eedacd
shikigami <mazenkassem101@gmail.com>
2026-06-30 15:16:52 +0300

    initial commit

---
+++ src/main/java/com/university/model/RegistrySystem.java
@@ -0,0 +1,32 @@
+package main.java.com.university.model;
+
+import java.util.HashMap;
+import java.util.List;
+
+public class RegistrySystem {
+    private HashMap<String, User> userRegistry;
+    private HashMap<String, Course> courseRegistry;
+
+    public void addUser(User user){
+        this.userRegistry.put(user.getId(), user);
+    }
+
+    public User getUser(String id){
+        return this.userRegistry.get(id);
+    }
+
+    public void addCourse(Course course){
+        this.courseRegistry.put(course.getCourseCode(), course);
+    }
+
+    public Course getCourse(String courseCode){
+        return this.courseRegistry.get(courseCode);
+    }
+
+    public void getAllCourses(){
+        for(String code : this.courseRegistry.keySet()){
+            var course = this.courseRegistry.get(code);
+            System.out.printf("- title: %s, code: %s, credits: %d, professor: %s, full: %b", course.getTitle(), course.getCourseCode(), course.getCredits(),course.getInstructor().getName(), course.isFull());
+        }
+    }
+}
